9aad1e234fa9c8d0d5e029639821675cf8d395e8
Shourya Shashank <shouryashashank@gmail.com>
2025-02-14 14:44:37 +0530

    Update python_notebook.py

--- app/predacons_agents/src/python_notebook.py
+++ app/predacons_agents/src/python_notebook.py
@@ -1,9 +1,19 @@
 
 import nbformat
 from nbclient import NotebookClient
+import asyncio
+
 class PythonNotebook:
     def __init__(self):
         self.nb = nbformat.v4.new_notebook()
+
+    async def initialize(self):
+        self.nb_client = NotebookClient(self.nb)
+        if self.nb_client.kc is None or not await self.nb_client.kc.is_alive():
+            self.nb_client.create_kernel_manager()
+            self.nb_client.start_new_kernel()
+            self.nb_client.start_new_kernel_client()
+
 
     def add_code_cell(self, code):
         self.nb.cells.append(nbformat.v4.new_code_cell(code))
@@ -11,13 +21,11 @@ class PythonNotebook:
         return len(self.nb.cells) - 1
 
     def execute(self):
-        client = NotebookClient(self.nb)
-        client.execute()
+        self.nb_client.execute()
         return self.nb
 
     def execute_cell(self, cell_index):
-        client = NotebookClient(self.nb)
-        client.execute_cell(cell_index=cell_index)
+        self.nb_client.execute_cell(self.nb.cells[cell_index], cell_index)
         return self.nb.cells[cell_index]
 
     def save(self, file_path):
